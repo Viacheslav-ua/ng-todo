78b1b8c4628f006ae55b12a94e54e608fe9b2143
Viacheslav-ua <atph@ukr.net>
2022-10-05 21:48:28 +0300

    finish lesson-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { TodoModule } from './modules/todo/todo.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      },
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
+    TodoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/todo/store/todo/todo.reducer.ts
@@ -0,0 +1,36 @@
+import { Todo } from "../../model/todu"
+import { TodoActions, todoActionsType } from "./todo.actions"
+
+export const TODO_REDUCER_NODE = 'todo'
+
+export interface TodoState {
+  idIncrement: number
+  todoList: Todo[]
+}
+
+const initialState: TodoState = {
+  idIncrement: 1,
+  todoList: [],
+}
+
+export const todoReducer = (state: TodoState = initialState, action: TodoActions) => {
+  switch (action.type) {
+    case todoActionsType.create:
+      return {
+        ...state,
+        idIncrement: state.idIncrement + 1,
+        todoList: [
+          ...state.todoList,
+          {
+            id: state.idIncrement,
+            name: action.payload,
+            completed: false,
+          }
+        ]
+      }
+
+    default:
+      return state
+  }
+
+}
